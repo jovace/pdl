e3d2e4bd319cf61b9468e8b41afb855a27db71c4
jovace <rmjuw21@gmail.com>
2018-11-27 16:10:47 +0100

    Cambio a Strings

--- src/pdl/analizadorLexico/analizadorSintactico.java
+++ src/pdl/analizadorLexico/analizadorSintactico.java
@@ -8,77 +8,82 @@ import java.util.Set;
 import java.util.Stack;
 
 public class analizadorSintactico {
-	Stack<Character> pila = new Stack<>();
-	Map<Character,Map<Character,ArrayList<Character>>> tablaTransicion = new HashMap<>();
-	Set<Character> terminales = new HashSet<>();
-	Set<Character> noTerminales = new HashSet<>();
-	Map<String,Character> tokenaTerminales = new HashMap<>();
+	Stack<String> pila = new Stack<>();
+	Map<String,Map<String,ArrayList<String>>> tablaTransicion = new HashMap<>();
+	Set<String> terminales = new HashSet<>();
+	Set<String> noTerminales = new HashSet<>();
+	Map<String,String> tokenaTerminales = new HashMap<>();
 	Map<Character,Token> terminalesAToken = new HashMap<>();
 	
 	public analizadorSintactico() {
-		terminales.add('a');
-		terminales.add('b');
-		terminales.add('c');
-		terminales.add('$');
+		terminales.add("a");
+		terminales.add("b");
+		terminales.add("c");
+		terminales.add("$");
 		
-		noTerminales.add('A');
-		noTerminales.add('B');
-		noTerminales.add('C');
-		noTerminales.add('D');
-		noTerminales.add('E');
-		noTerminales.add('F');
+		noTerminales.add("A");
+		noTerminales.add("B");
+		noTerminales.add("C");
+		noTerminales.add("D");
+		noTerminales.add("E");
+		noTerminales.add("F");
 		
-		ArrayList<Character> produccionAa = new ArrayList<>();
-		Map<Character, ArrayList<Character>> filaA = new HashMap<>();
-		produccionAa.add('a');
-		produccionAa.add('D');
-		filaA.put('a',produccionAa);
-		tablaTransicion.put('A', filaA);
+		ArrayList<String> produccionAa = new ArrayList<>();
+		Map<String, ArrayList<String>> filaA = new HashMap<>();
+		produccionAa.add("a");
+		produccionAa.add("D");
+		filaA.put("a",produccionAa);
+		tablaTransicion.put("A", filaA);
 		
-		ArrayList<Character> produccionDa = new ArrayList<>();
-		ArrayList<Character> produccionDb = new ArrayList<>();
-		Map<Character, ArrayList<Character>> filaD = new HashMap<>();
-		produccionDa.add('A');
-		filaD.put('a', produccionDa);
-		produccionDb.add('B');
-		filaD.put('b', produccionDb);
-		tablaTransicion.put('D', filaD);
+		ArrayList<String> produccionDa = new ArrayList<>();
+		ArrayList<String> produccionDb = new ArrayList<>();
+		Map<String, ArrayList<String>> filaD = new HashMap<>();
+		produccionDa.add("A");
+		filaD.put("a", produccionDa);
+		produccionDb.add("B");
+		filaD.put("b", produccionDb);
+		tablaTransicion.put("D", filaD);
 		
-		ArrayList<Character> produccionBb = new ArrayList<>();
-		Map<Character, ArrayList<Character>> filaB = new HashMap<>();
-		produccionBb.add('b');
-		produccionBb.add('E');
-		filaB.put('b', produccionBb);
-		tablaTransicion.put('B', filaB);
+		ArrayList<String> produccionBb = new ArrayList<>();
+		Map<String, ArrayList<String>> filaB = new HashMap<>();
+		produccionBb.add("b");
+		produccionBb.add("E");
+		filaB.put("b", produccionBb);
+		tablaTransicion.put("B", filaB);
 		
-		ArrayList<Character> produccionEb = new ArrayList<>();
-		ArrayList<Character> produccionEc = new ArrayList<>();
-		Map<Character, ArrayList<Character>> filaE = new HashMap<>();
-		produccionEb.add('B');
-		filaE.put('b',produccionEb);
-		produccionEc.add('C');
-		filaE.put('c', produccionEc);
-		tablaTransicion.put('E', filaE);
+		ArrayList<String> produccionEb = new ArrayList<>();
+		ArrayList<String> produccionEc = new ArrayList<>();
+		Map<String, ArrayList<String>> filaE = new HashMap<>();
+		produccionEb.add("B");
+		filaE.put("b",produccionEb);
+		produccionEc.add("C");
+		filaE.put("c", produccionEc);
+		tablaTransicion.put("E", filaE);
 		
-		ArrayList<Character> produccionCc = new ArrayList<>();
-		Map<Character, ArrayList<Character>> filaC = new HashMap<>();
-		produccionCc.add('c');
-		produccionCc.add('F');
-		filaC.put('c', produccionCc);
-		tablaTransicion.put('C', filaC);
+		ArrayList<String> produccionCc = new ArrayList<>();
+		Map<String, ArrayList<String>> filaC = new HashMap<>();
+		produccionCc.add("c");
+		produccionCc.add("F");
+		filaC.put("c", produccionCc);
+		tablaTransicion.put("C", filaC);
 		
-		ArrayList<Character> produccionFc = new ArrayList<>();
-		ArrayList<Character> produccionF$ = new ArrayList<>();
-		Map<Character, ArrayList<Character>> filaF = new HashMap<>();
-		produccionFc.add('C');
-		filaF.put('c', produccionFc);
+		ArrayList<String> produccionFc = new ArrayList<>();
+		ArrayList<String> produccionF$ = new ArrayList<>();
+		Map<String, ArrayList<String>> filaF = new HashMap<>();
+		produccionFc.add("C");
+		filaF.put("c", produccionFc);
 		produccionF$.clear();
-		filaF.put('$', produccionF$);
-		tablaTransicion.put('F', filaF);
+		filaF.put("$", produccionF$);
+		tablaTransicion.put("F", filaF);
 		
 		
-		
-		boolean bol=analizar("aabc$");
+		ArrayList<String> tokens=new ArrayList<>();
+		tokens.add("a");
+		tokens.add("a");
+		tokens.add("b");
+		tokens.add("c");
+		tokens.add("$");
+		boolean bol=analizar(tokens);
 		System.out.println(bol);
 		//Construccion de tabla de transiciones
 		
@@ -90,17 +95,17 @@ public class analizadorSintactico {
 	 * Lo transformamos en sus correspondientes simbolos terminales.
 	 * Analizamos sintacticamente
 	 */
-	public boolean analizar(/*ArrayList<Token>*/String listaTokens) {
+	public boolean analizar(/*ArrayList<Token>*/ArrayList<String> listaTokens) {
 		
-		String codigo=listaTokens;//convertirTokenaTerminales(listaTokens);
+		ArrayList<String> codigo=listaTokens;//convertirTokenaTerminales(listaTokens);
 		pila.clear();
-		pila.push('$');
-		pila.push('A');
+		pila.push("$");
+		pila.push("A");
 		int puntero=0;
 		
-		while(pila.peek()!='$') {
-			Character X=pila.peek();
-			Character a=codigo.charAt(puntero);
+		while(!pila.peek().equals("$")) {
+			String X=pila.peek();
+			String a=codigo.get(puntero);
 			if(terminales.contains(X)) {
 				if(X.equals(a)) {
 					pila.pop();
@@ -111,7 +116,7 @@ public class analizadorSintactico {
 			}else if(noTerminales.contains(X)) {
 				pila.pop();
 				
-				ArrayList<Character> produccion = tablaTransicion.get(X).get(a);
+				ArrayList<String> produccion = tablaTransicion.get(X).get(a);
 				for(int i=produccion.size()-1;i>=0;i--) {					
 					pila.push(produccion.get(i));
 				}
