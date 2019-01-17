87510efcad59943dd018eca6cb4bd8146904200e
Jorge <rmjuw21@gmail.com>
2019-01-17 18:07:56 +0100

    .

--- casosPrueba/correcto1/entrada/codigo.js
+++ casosPrueba/correcto1/entrada/codigo.js
@@ -21,12 +21,20 @@ function int div(int x, int y){
 }
 
 var int e=suma(a,b);
+print("Resultado esperado es 3");
 print(e);
+print("Resultado esperado es false");
 print(e<3);
+print("Resultado esperado es true");
 print(e<=3);
+print("Resultado esperado es true");
 print(e==3);
+print("Resultado esperado es true");
 print(e>=3);
+print("Resultado esperado es false");
 print(e>3);
 var int f=resta(d,c);
+print("Resultado esperado es 3");
 print(div(e,f));
+print("Resultado esperado es 8");
 print(mult(suma(a,c),resta(d,b)));
